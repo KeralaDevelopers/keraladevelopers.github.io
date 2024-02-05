7dadd726a04cb67a19b9855793e05e1bd26f71f2
Sabu Siyad <hello@ssiyad.com>
2024-02-06 01:21:14 +0530

    feat: base level catch all page
    also,
    - How to ask in malayalam
    - Manjari

---
+++ pages/[_].vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="m-auto max-w-prose">
+    <ContentDoc v-slot="{ doc }">
+      <UBreadcrumb
+        divider="/"
+        :links="[{ label: 'Home', to: '/' }, { label: doc.title }]"
+        class="mb-4"
+      />
+      <div class="prose prose-slate prose-pre:bg-slate-100">
+        <h1>{{ doc.title }}</h1>
+        <ContentRenderer :value="doc" />
+      </div>
+    </ContentDoc>
+  </main>
+</template>
