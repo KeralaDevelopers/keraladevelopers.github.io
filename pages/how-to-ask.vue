96ffa6ea1520f847b7bff0d57f307d9639440f28
Sabu Siyad <hello@ssiyad.com>
2024-02-06 22:22:39 +0530

    fix(doc): `how-to-ask`: dark: prose-invert

--- pages/how-to-ask.vue
+++ pages/how-to-ask.vue
@@ -6,7 +6,7 @@
         :links="[{ label: 'Home', to: '/' }, { label: doc.title }]"
         class="mb-4"
       />
-      <div class="prose prose-slate prose-pre:bg-slate-100">
+      <div class="prose prose-slate dark:prose-invert prose-pre:bg-slate-100">
         <h1>{{ doc.title }}</h1>
         <ContentRenderer :value="doc" />
       </div>
