dfac509c79615f596f2346efe83e60be4cbd96cf
Sabu Siyad <hello@ssiyad.com>
2024-02-06 22:31:47 +0530

    chore: home page wordings

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="flex size-full items-center">
     <div class="max-w-4xl">
-      <div class="mb-4 text-6xl font-bold text-slate-800 dark:text-slate-200">
-        <span> Connecting developers from </span>
+      <div class="mb-4 text-5xl font-bold text-slate-800 dark:text-slate-200">
         <span class="text-brand-700 dark:text-brand-200">Kerala</span>
+        <span class="text-brand-500"> &hearts; </span>
+        <span class="text-brand-700 dark:text-brand-200">Devs</span>
       </div>
       <div class="mb-4 text-xl font-medium text-slate-700 dark:text-slate-300">
-        We are a community of developers, designers, and tech enthusiasts from
-        Kerala. Join us to connect, learn, and grow together, while sipping some
-        tender coconut water.
+        We are a collective of tech enthusiasts, developers, and designers
+        hailing from Kerala. Come be a part of our community to forge
+        connections, gain knowledge, and thrive together &mdash; all while
+        enjoying refreshing sips of tender coconut water.
       </div>
       <a href="/telegram" target="_blank">
         <UButton size="xl" variant="solid" label="Join us">
